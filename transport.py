e75ca6c248cf94bfa9bad60e4fb447735de06aea
Łukasz Kułakowski <53407982+kulakowski-lukasz@users.noreply.github.com>
2021-04-15 21:34:51 +0200

    Create transport.py

---
+++ transport.py
@@ -0,0 +1,22 @@
+class Wheeler:
+    
+    def __init__(self):
+        pass
+
+    def roll_forward(self):
+        print("Rolling forward")
+
+    def roll_backwards(self):
+        print("Rolling backwards")
+
+class Swimmer:
+    
+    def __init__(self):
+        pass
+
+    def swim_forward(self):
+        print("Swimming forward")
+
+    def swim_backwards(self):
+        print("Swimming backwards") 
+
