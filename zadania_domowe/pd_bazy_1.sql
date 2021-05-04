150d626e2167b6fb314d45b498ae41b5ab2dfa14
Łukasz Kułakowski <53407982+kulakowski-lukasz@users.noreply.github.com>
2021-05-04 22:02:21 +0200

    Revert "Homework 1 revised for MYSQL"
    This reverts commit d12426087676efd087f760221d336ef5a81d83ee.

--- zadania_domowe/pd_bazy_1.sql
+++ zadania_domowe/pd_bazy_1.sql
@@ -1,27 +1,25 @@
--- Revised for the MYSQL
-
 CREATE TABLE users (
-    user_id INT NOT NULL PRIMARY KEY,
-    login varchar(255) DEFAULT NULL,
-    password varchar(255) DEFAULT NULL
+    user_id INTEGER PRIMARY KEY,
+    login TEXT,
+    password TEXT
 );
 
 CREATE TABLE categories (
-    category_id INT NOT NULL PRIMARY KEY,
-    category VARCHAR(200) NOT NULL,
-    user_id INT NOT NULL,
-    fOREIGN KEY (user_id) REFERENCES users(user_id) ON UPDATE CASCADE ON DELETE RESTRICT
+    category_id INTEGER PRIMARY KEY,
+    category TEXT,
+    user_id INTEGER,
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
 
 CREATE TABLE expenses (
-    expense_id INT NOT NULL PRIMARY KEY,
-    amount DECIMAL(12, 2) NOT NULL,
-    category_id INT NOT NULL,
-    date DATETIME NOT NULL,
-    comment VARCHAR(255),
-    user_id INT NOT NULL,
-    FOREIGN KEY (category_id) REFERENCES categories(category_id) ON UPDATE CASCADE ON DELETE RESTRICT,
-    FOREIGN KEY (user_id) REFERENCES users(user_id) ON UPDATE CASCADE ON DELETE RESTRICT
+    expense_id INTEGER PRIMARY KEY,
+    amount REAL,
+    category_id INTEGER,
+    date INTEGER,
+    comment TEXT,
+    user_id INTEGER,
+    FOREIGN KEY (category_id) REFERENCES categories(category_id),
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
 
 -- TODO:
